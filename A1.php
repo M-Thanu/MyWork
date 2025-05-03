1bea5da393586e15531d4e7d010985331ba7fffd
M-Thanu <mahanthanuja@gmail.com>
2025-05-03 13:04:01 +0530

    activated php

---
+++ A1.php
@@ -0,0 +1,33 @@
+<?php
+    
+    $var1=13;
+    $var2=3.5;
+    $var3="23";
+    $var4="5.97";
+    $txt=10;
+    $txt1="server side";
+
+    $var6 = $var1 + $txt;
+    echo $var6;
+    echo "<br>";
+
+    echo "$var1";
+    echo "<br>";
+    echo '$var1';
+    echo "<br>";
+
+    $var5 = $var1 + $var2;
+    echo $var5;
+    echo "<br>";
+       $var8 = $txt.$txt1;
+    echo $var8;
+    echo "<br>";
+
+    $var7 = $var1 + $txt;
+    echo $var7;
+    echo "<br>";
+
+   
+
+?>
+
---
+++ A2.php
@@ -0,0 +1,13 @@
+<?php
+    $m1=90;
+    $m2=60;
+    $m3=40;
+
+    $avg=($m1+$m2+$m3)/3;
+    if($avg>50.00){
+        echo 'Pass';
+    }else{
+        echo 'Fail';
+    }
+
+?>
